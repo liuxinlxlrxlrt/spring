4c6b51a8eb15b5fd1dcabc26b157382bb4112d28
liuxin <183771798@qq.com>
2020-11-04 23:23:26 +0800

    2

--- spring01/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
+++ spring01/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
@@ -3,6 +3,19 @@ package com.itheima.dao.impl;
 import com.itheima.dao.UserDao;
 
 public class UserDaoImpl implements UserDao {
+
+    public UserDaoImpl() {
+        System.out.println("UserDaoImpl创建...");
+    }
+
+    public void init(){
+        System.out.println("初始化方法。。。");
+    }
+
+    public void destroy(){
+        System.out.println("销毁方法。。。");
+    }
+
     @Override
     public void save() {
         System.out.println("sava running......");
